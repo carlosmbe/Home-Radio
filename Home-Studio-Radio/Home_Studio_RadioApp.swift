8281fdfdfc8640d5d9caa8a615bf9abbe034c0b5
Carlos Mbendera <carlosmbendera@gmail.com>
2024-04-16 15:34:18 -0500

    Initial Commit

---
+++ Home-Studio-Radio/Home_Studio_RadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Studio_RadioApp.swift
+//  Home-Studio-Radio
+//
+//  Created by Carlos Mbendera on 16/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Home_Studio_RadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
